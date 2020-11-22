c4173e2153b9660b38787bd6c77829cf1ca1ab9a
sohamderoy <sohamderoy.iitk@gmail.com>
2020-11-22 20:28:18 +0530

    minor updates

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/signout", (req, res) => (res.send("User Signout")));
+const signout = (req, res) => (
+    res.json({
+        message: "User Signout"
+    })
+);
+router.get("/signout", signout);
 
 module.exports = router;
