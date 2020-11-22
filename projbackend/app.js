0909b665a715296041b5ae265ae16db71395970f
sohamderoy <sohamderoy.iitk@gmail.com>
2020-11-22 18:53:56 +0530

    added signout get route

--- projbackend/app.js
+++ projbackend/app.js
@@ -2,9 +2,15 @@ require('dotenv').config();
 
 const mongoose = require("mongoose");
 const express = require("express");
-
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+const authRoutes = require("./routes/auth")
+
+
+// DB connection
 mongoose.connect("mongodb://localhost:27017/tshirt", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -13,12 +19,19 @@ mongoose.connect("mongodb://localhost:27017/tshirt", {
     console.log("DB CONNECTED...")
 });
 
+// Inbuilt middleware 
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//Routes
+app.use("/api", authRoutes);
 
 
-
-
-
+// Ports
 const port = process.env.PORT || 8000;
+
+// Starting a server
 app.listen(port, () => {
     console.log(`app is running at: ${port}`)
 });
