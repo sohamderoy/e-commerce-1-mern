10e728fed755d920ccbb01ade2f8944846424f76
sohamderoy <sohamderoy.iitk@gmail.com>
2020-11-22 16:21:52 +0530

    testing some middleware code

--- testback/index.js
+++ testback/index.js
@@ -11,6 +11,15 @@ const app = express();
 // })
 
 const port = 8000;
+
+const admin = (req, res) => (res.send("This is admin page"));
+const isAdmin = (req, res, next) => {  // what dose the middleware isAdmin do???
+    console.log("isAdmin is running");
+    next();                             // mention the next in the arguments above to use next()
+}
+//the isAdmin middleware function can be crafted in a way so that only if its true then the next() should execute o else we stop the request and do not sent any response
+// we can add multiple middleware if we want in same way
+app.get("/admin", isAdmin, admin); // Here isAdmin is running as a middleware whose process has been defined above
 app.get("/login", (req, res) => (res.send("You are visiting login route!")));
 app.get("/", (req, res) => (res.send("Its the home page")));
 app.get("/signup", (req, res) => (res.send("Its the signup page")));
