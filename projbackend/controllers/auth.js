ad5294ff69a0fa28919a91b61560cebfbe6956d8
sohamderoy <sohamderoy.iitk@gmail.com>
2020-11-23 19:37:32 +0530

    Updated controller/auth.js

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -10,7 +10,11 @@ exports.signup = (req, res) => {
                 err: "Not able to save user in DB"
             })
         }
-        res.json(user);
+        res.json({
+            name: user.name,
+            email: user.email,
+            id: user._id
+        });
     });
 }; 
 
